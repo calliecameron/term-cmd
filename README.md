5e7a86d1f0e663154f350ae6521b336781a044a0
Callum Cameron <cjcameron7@gmail.com>
2014-08-01 22:26:00 +0100

    Fixed the documentation.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ In vanilla Emacs, programs running in the terminal emulator can send commands ba
 
 This package lets you add new commands. It uses a different magic escape sequence to avoid interfering with the built-in commands, but the principle is the same. When a program prints a command, it won't show up on the screen, but will be interpreted by Emacs instead.
 
-Note: this is a library, that doesn't make any user-visible changes. For an example of something that actually uses it, see term-alert-mode.
+Note: this is a library, and doesn't make any user-visible changes. For an example of something that actually uses it, see [term-alert](https://github.com/CallumCameron/term-alert).
 
 
 Installation
@@ -47,17 +47,10 @@ Because the commands are based on terminal output, they work just as well throug
 License
 -------
 
-    Copyright (C) 2014 Callum J. Cameron
+Copyright (C) 2014 Callum J. Cameron
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- term-cmd.el
+++ term-cmd.el
@@ -2,8 +2,10 @@
 
 ;; Copyright (C) 2014 Callum J. Cameron
 
-;; Author: Callum Cameron <cjcameron7@gmail.com>
-;; Keywords: terminal
+;; Author: Callum J. Cameron <cjcameron7@gmail.com>
+;; Keywords: processes
+
+;; This file is not part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -20,7 +22,60 @@
 
 ;;; Commentary:
 
-;; 
+;; Send commands to Emacs from programs running in the term.el
+;; terminal emulator.
+;;
+;; In vanilla Emacs, programs running in the terminal emulator can
+;; send commands back to Emacs by printing a 'magic escape sequence'
+;; which the terminal emulator parses -- this is how directory
+;; tracking works.  But the list of commands is hard-coded, and you
+;; can't add new ones.
+;;
+;; This package lets you add new commands.  It uses a different magic
+;; escape sequence to avoid interfering with the built-in commands,
+;; but the principle is the same.  When a program prints a command, it
+;; won't show up on the screen, but will be interpreted by Emacs
+;; instead.
+;;
+;; Note: this is a library, and doesn't make any user-visible changes.
+;; For an example of something that actually uses it, see the
+;; 'term-alert' package (https://github.com/CallumCameron/term-alert).
+;;
+;; Installation:
+;;
+;; 1. Make sure this file is on your load path
+;; 2. (require 'term-cmd)
+;;
+;; There is also a shell script, 'emacs-term-cmd', in this package's
+;; git repository, which makes sending commands easier.
+;;
+;; Usage:
+;;
+;; To register a command:
+;;     (add-to-list 'term-cmd-commands-alist
+;;                  '("command" . some-callback-function))
+;; where "command" is the name of the command, and
+;; some-callback-function is the function you want to be called when
+;; the command is run.  The function should take two arguments -- the
+;; first is the command name itself, and the second is the command's
+;; argument.
+;;
+;; To send a command, just print the magic escape sequence in some
+;; program in the terminal:
+;;     # Using GNU printf
+;;     /usr/bin/printf '\eTeRmCmD %s %s\n' "${CMD}" "${ARG}"
+;;
+;;     # With a shell built-in, the syntax might be different; here for ZSH:
+;;     print "\033TeRmCmD ${CMD} ${ARG}"
+;;
+;; The '\e' and '\033' are the ESC character.  You can leave off the
+;; argument if you don't need it.
+;;
+;; Alternatively, use the 'emacs-term-cmd' script:
+;;     emacs-term-cmd command arg
+;;
+;; Because the commands are based on terminal output, they work just
+;; as well through nested shells, or multiple SSH sessions.
 
 ;;; Code:
 
@@ -33,18 +88,18 @@ command to match on, and func takes two args, the command and the
 command's argument.  To run a command from the terminal, output a line
 of the form '\\eTeRmCmD <command> <arg>\\n', where arg is an arbitrary
 string; the function with key <command> will be called with command
-and arg.")
+and arg.  Arg can also be omitted if it is not required.")
 
 
 ;; These variables allow incomplete commands at the end of input to be
-;; stored and handled when more input arrives
+;; stored and handled when more input arrives.
 (defvar term-cmd--partial-cmd nil)
 (make-local-variable 'term-cmd--partial-cmd)
 
 ;; These variables do the same thing, but for Emacs' built-in
 ;; directory-tracking messages; see
 ;; term-cmd--ansi-partial-beginning-check and
-;; term-cmd--ansi-partial-end-check, below
+;; term-cmd--ansi-partial-end-check, below.
 (defvar term-cmd--partial-ansi-terminal-message nil)
 (make-local-variable 'term-cmd--partial-ansi-terminal-message)
 
